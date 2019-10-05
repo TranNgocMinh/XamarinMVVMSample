d582c2c5acf78c912bc8fdc70d4d8bb9587b817a
NgocMinhTran <tranngocminhcdn@gmail.com>
2019-10-05 10:14:32 +0700

    Add project files.

---
+++ MVVMSample/MVVMSample/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMSample.ViewModel;
+using Xamarin.Forms;
+
+namespace MVVMSample
+{
+	public partial class MainPage : ContentPage
+	{
+        private PersonViewModel ViewModel { get; set; }
+        public MainPage()
+		{
+			InitializeComponent();
+
+            this.ViewModel = new PersonViewModel();
+            this.BindingContext = this.ViewModel;
+        }
+	}
+}
